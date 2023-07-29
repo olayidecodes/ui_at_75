e4594876cb0c1e7c77f76e3b41c8e659ed2d4571
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-07-29 12:41:56 +0100

    modal screen update

--- src/components/modal/screen2/Amount.tsx
+++ src/components/modal/screen2/Amount.tsx
@@ -7,26 +7,47 @@ const Amount = () => {
       <form className={style.form}>
         <div className={style.field1}>
           <label htmlFor="purpose">Purpose of payment</label>
-          <input type="text" name='purpose' />
+          <input className={style.input} type="text" name='purpose' />
         </div>
 
         <div className={style.fields}>
           <div className={style.field1}>
             <label htmlFor="purpose">Amount to pay</label>
-            <div><span>&#8358;</span><input type="text" name='purpose' /></div>
+            <div className={style.naira}>
+              <span className={style.nairalogo}>&#8358;</span>
+              <input className={style.input} type="text" name='purpose' />
+            </div>
           </div>
           <div className={style.field1}>
             <label htmlFor="purpose">Select Currency</label>
-            <select>
-              <option>Select Currency</option>
-              <option>USD</option>
-              <option>NGN</option>
-              <option>GBP</option>
-              <option>SAUD</option>
-              <option>EUR</option>
+            <select className={style.input}>
+              <option>NGN - Nigerian Naira</option>
+              <option>CAD - Canidian Dollar</option>
+              <option>USD - United State Dollar</option>
+              <option>GBP - British Pound</option>
+              <option>AUD - Australian Dollar</option>
+              <option>EUR - Euro</option>
             </select>
           </div>
         </div>
+
+        <div className={style.btns}>
+            <button 
+              type='submit' 
+              className={style.next} 
+              // onClick={() => onButtonClick("pageone")}
+              >
+                Next
+              </button>
+
+              <button 
+              type='submit' 
+              className={style.previous} 
+              // onClick={() => onButtonClick("pagetwo")}
+              >
+              Previous
+            </button>
+          </div>
       </form>
     </main>
   )
