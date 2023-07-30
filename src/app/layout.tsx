70f0b86052a28da039321350821a01f923914aa3
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-07-30 19:22:18 +0100

    updated responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={open_sans.className}>
         <div className="container">
+          <Navbar />
           {children}
           <PersonalDetails/>
           <Amount/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         <h1>Progress made so far</h1>
         <BarChart />
         <ProgressBar />
-        <Gallery />
       </section>
+      <Gallery />
     </main>
   )
 }
